357cd4bf652fd5191a2a5079096a89fa42c59112
prodonmajor <killmewithdlord@gmail.com>
2015-03-25 13:44:48 +0100

    Added few lines of code to accept user inputs

--- src/javarepo/SecondJavaRepo.java
+++ src/javarepo/SecondJavaRepo.java
@@ -14,5 +14,9 @@ import javax.swing.JOptionPane;
 public class SecondJavaRepo {
 public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Welcome to git tutorial using netbeans");
+       String sname= JOptionPane.showInputDialog("Your surname please");
+        String oname= JOptionPane.showInputDialog("Your last name please");
+        String fname= sname+" "+oname;
+        JOptionPane.showMessageDialog(null, "Your fullname is"+" "+fname);
     } 
 }
